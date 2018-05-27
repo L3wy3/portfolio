642f9fbe940094127ca6c394077a723f0d79e641
l3wy3 <lstowning@outlook.com>
2018-05-27 19:05:06 +0100

    js/css tweask

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,9 +3,17 @@ $(window).scroll(function(event){
     var st = $(this).scrollTop();
     if(st > lastScrollTop){
     $(".nav-hider").addClass("hide");
+      console.log("hide");
     }
     else{
       $(".nav-hider").removeClass("hide");
+      console.log("show");
     }
     lastScrollTop = st;
+});
+$( ".carousel-control-prev" ).click(function() {
+  $(".nav-hider").addClass("hide");
+});
+$( ".carousel-control-next" ).click(function() {
+  $(".nav-hider").addClass("hide");
 });
