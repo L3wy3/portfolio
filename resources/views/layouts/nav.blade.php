f25e330965eee40595b73f44d088a3a0bcda8792
l3wy3 <lstowning@outlook.com>
2018-05-30 22:19:53 +0100

    nav fix

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,12 +1,14 @@
 <div class="nav-div">
   <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand padding-l-50" href="/">Home</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarNav">
 <ul class="navbar-nav">
+<li class="nav-item">
+<a class="navbar-link" href="/">Home</a>
+</li>
 <li class="nav-item">
 <a class="nav-link" href="/blog">Blog</a>
 </li>
@@ -19,22 +21,6 @@
 <li class="nav-item">
 <a class="nav-link" href="/contact">Contact</a>
 </li>
-<div style="position:absolute;right:50px;display:inline-flex;">
-@if (Auth::check())
-<li class="nav-item">
-<a class="nav-link" href="/blog">{{ Auth::user()->name }}</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/logout">Log out</a>
-</li>
-@else
-<li class="nav-item">
-<a class="nav-link" href="/register">Register</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="/login">Log in</a>
-</li>
-@endif
 </ul>
 </div>
 </nav>
