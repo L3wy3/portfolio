1b24d8c52edfa3a941c19d30b66e16233b0dafa1
l3wy3 <lstowning@outlook.com>
2018-05-30 22:41:52 +0100

    blregh

--- resources/views/work/index.blade.php
+++ resources/views/work/index.blade.php
@@ -4,9 +4,10 @@
 @include('layouts.banner')
 <div id='project-div' class="project-div">
   <div class='container'>
+    <h2 style="text-align:center; margin:50px;">Here is a selection of projects I've either done soley or been the main developer of over the last few years, you can sort them by various tags</h2>
     <div class="row">
       @foreach($projects as $project)
-      <div class="col-sm-4">
+      <div class="col-md-6 col-lg-4">
         @include('work.project')
       </div>
       @endforeach
