79c9faa80ae5ad5263eced6207629ceeeb0df25e
l3wy3 <lstowning@outlook.com>
2018-05-31 19:09:16 +0100

    form stuff

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,13 +4,20 @@
 @include('layouts.banner')
 <div class="container">
   <div class="col-sm-6" style="background-color:blue; height:600px;">
-    <form class="" action="index.html" method="post">
-      formy form
-      <div class="form-control">
-        <label for=""></label>
-        <input type="text" name="" value="">
+    {!! Form::open(['route' -> 'contact.index'] !!)}
+      <div class="form-group">
+        {!! Form::label('name', 'Your Name') !!}
+        {!! Form::text('name' , null. ['class' => 'form-control']) !!}
       </div>
-    </form>
+      <div class="form-group">
+        {!! Form::label('email', 'E-mail Address') !!}
+        {!! Form::text('name' , null. ['class' => 'form-control']) !!}
+      </div>
+      <div class="form-group">
+        {!! Form::textarea('msg' , null. ['class' => 'form-control']) !!}
+      </div>
+      {!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
+      {!! Form::close() !!}
     </div>
     <div class="col-sm-6">
       blah blah blah
--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,13 @@
 |
 */
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/news', function () {
-    return view('news');
-});
-
-Route::get('/news/create', 'PostsController@create');
-=======
 Route::get('/', 'Controller@index');
 
 Route::get('/blog', 'PostsController@index')->name('home');
 Route::get('/blog/create','PostsController@create');
-Route::post('/blog','PostsController@store');
+//Route::post('/blog','PostsController@store');
 Route::get('/blog/{post}','PostsController@show');
-Route::post('/blog/{post}', 'CommentsController@store');
+//Route::post('/blog/{post}', 'CommentsController@store');
 
 Route::get('/contact', 'ContactController@index');
 Route::post('/contact', 'ContactController@store');
@@ -43,4 +32,3 @@ Route::get('/hobbies', 'HobbiesController@index');
 Route::get('/login', 'SessionsController@create');
 Route::post('/login', 'SessionsController@store');
 Route::get('/logout', 'SessionsController@destroy');
->>>>>>> 2143ba06f1d493ead94ad48002ac6e5daf5a15b6
