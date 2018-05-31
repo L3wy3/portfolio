cd9c9806b07e69f23fb4a5db2250c96811a68765
l3wy3 <lstowning@outlook.com>
2018-05-31 20:18:50 +0100

    moo

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,7 +4,7 @@
 @include('layouts.banner')
 <div class="container">
   <div class="col-sm-6" style="background-color:blue; height:600px;">
-    {!! Form::open(['route' -> 'contact.index'] !!)}
+    {!! Form::open(['route' -> 'contact.store'] !!)}
       <div class="form-group">
         {!! Form::label('name', 'Your Name') !!}
         {!! Form::text('name' , null. ['class' => 'form-control']) !!}
