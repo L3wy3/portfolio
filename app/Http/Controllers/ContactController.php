a8346ba057fd50d8d1cfe507ada66bd92b9575b1
l3wy3 <lstowning@outlook.com>
2018-06-02 22:18:41 +0100

    moo

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,11 +10,10 @@ class ContactController extends Controller
       $pagetitle = "contact";
       return view('contact.index', compact('pagetitle'));
     }
-    public function store(Request $request){
-
-      $admin->notify(ContactFormRequest $message, Admin $admin);
+    public function store(ContactFormRequest $message, Admin $admin)
+	{        //send the admin an notification
+		$admin->notify(new InboxMessage($message));
 		// redirect the user back
 		return redirect()->back()->with('message', 'thanks for the message! We will get back to you soon!');
-
-    }
+	}
 }
