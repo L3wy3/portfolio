e95203c685336789cd1f12a18b42f7718de09853
l3wy3 <lstowning@outlook.com>
2018-06-02 22:20:56 +0100

    contact form

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -4,20 +4,27 @@
 @include('layouts.banner')
 <div class="container">
   <div class="col-sm-6" style="background-color:blue; height:600px;">
-    {!! Form::open(['route' -> 'contact.store'] !!)}
-      <div class="form-group">
-        {!! Form::label('name', 'Your Name') !!}
-        {!! Form::text('name' , null. ['class' => 'form-control']) !!}
-      </div>
-      <div class="form-group">
-        {!! Form::label('email', 'E-mail Address') !!}
-        {!! Form::text('name' , null. ['class' => 'form-control']) !!}
-      </div>
-      <div class="form-group">
-        {!! Form::textarea('msg' , null. ['class' => 'form-control']) !!}
-      </div>
-      {!! Form::submit('Submit', ['class' => 'btn btn-info']) !!}
-      {!! Form::close() !!}
+    <form class="form-horizontal" method="POST" action="/contact">
+			{{ csrf_field() }}
+			<div class="form-group">
+			<label for="Name">Name: </label>
+			<input type="text" class="form-control" id="name" placeholder="Your name" name="name" required>
+		</div>
+
+		<div class="form-group">
+			<label for="email">Email: </label>
+			<input type="text" class="form-control" id="email" placeholder="john@example.com" name="email" required>
+		</div>
+
+		<div class="form-group">
+			<label for="message">message: </label>
+			<textarea type="text" class="form-control luna-message" id="message" placeholder="Type your messages here" name="message" required></textarea>
+		</div>
+
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary" value="Send">Send</button>
+			</div>
+		</form>
     </div>
     <div class="col-sm-6">
       blah blah blah
