7adf8a1017f1330a6be10edab88837c85c6936a7
l3wy3 <lstowning@outlook.com>
2018-06-03 11:26:13 +0100

    f

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,13 +1,12 @@
 <aside class="col-md-4 blog-sidebar">
   <div class="p-3 mb-3 bg-light rounded">
     <h4 class="font-italic">About</h4>
-    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    <p class="mb-0">I obviously had to take some creative license with the dates of this blog as I couldn't maintain this blog before building it.</p>
   </div>
 
   <div class="p-3">
     <h4 class="font-italic">Archives</h4>
     <ol class="list-unstyled mb-0">
-      <p>for each differant month representated</p>
       @foreach ($archives as $stats)
       <li>
         <a href="/blog/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">
@@ -15,27 +14,14 @@
         </a>
       </li>
       @endforeach
-      <!-- <li><a href="#">March 2014</a></li>
-      <li><a href="#">February 2014</a></li>
-      <li><a href="#">January 2014</a></li>
-      <li><a href="#">December 2013</a></li>
-      <li><a href="#">November 2013</a></li>
-      <li><a href="#">October 2013</a></li>
-      <li><a href="#">September 2013</a></li>
-      <li><a href="#">August 2013</a></li>
-      <li><a href="#">July 2013</a></li>
-      <li><a href="#">June 2013</a></li>
-      <li><a href="#">May 2013</a></li>
-      <li><a href="#">April 2013</a></li> -->
     </ol>
   </div>
 
   <div class="p-3">
     <h4 class="font-italic">Elsewhere</h4>
     <ol class="list-unstyled">
-      <li><a href="#">GitHub</a></li>
-      <li><a href="#">Twitter</a></li>
-      <li><a href="#">Facebook</a></li>
+      <li><a target="_blank" href="https://github.com/L3wy3">GitHub</a></li>
+      <li><a target="_blank" href="https://www.instagram.com/lewster1/">Instagram</a></li>
     </ol>
   </div>
 </aside><!-- /.blog-sidebar -->
