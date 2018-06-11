51167cb013ad208f15193c02acee1f031e7bee7a
lewy <lstowning@outlook.com>
2018-06-11 23:20:51 +0100

    removed contact page form menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,9 +18,9 @@
 <li class="nav-item">
 <a class="nav-link" href="/hobbies">Hobbies</a>
 </li>
-<li class="nav-item">
+<!-- <li class="nav-item">
 <a class="nav-link" href="/contact">Contact</a>
-</li>
+</li> -->
 </ul>
 </div>
 </nav>
--- resources/views/work/index.blade.php
+++ resources/views/work/index.blade.php
@@ -6,6 +6,7 @@
   <div class='container'>
     <h2 style="text-align:center; margin:50px;">Here is a selection of projects I've either done soley or been the main developer of over the last few years, you can sort them by various tags</h2>
     <div class="row">
+      <a href="?tag">hwthg</a>
       @foreach($projects as $project)
       <div class="col-md-6 col-lg-4">
         @include('work.project')
