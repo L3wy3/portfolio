8c759414120408a1c784a52123018b292fdb8d46
lewy <lstowning@outlook.com>
2018-06-13 00:04:55 +0100

    page styling

--- resources/views/work/project.blade.php
+++ resources/views/work/project.blade.php
@@ -1,63 +1,22 @@
 <div class="project-hide transparent">
 @if($project->link)
-	<a href='{{ $project->link }}'>
+<a target='_blank' href='{{ $project->link }}'>
 @endif
 <style media="screen">
-	.lang-block img{width:30px;}
+.lang-block img{width:30px;}
 </style>
+<p class="card-text-title">{{$project->title}}</p>
+<p class="card-text-title" style="margin:5px 0;">{{ $project->company }}</p>
 <div class="">
-  <div style="background-image:url('{{ $project->img }}');" class="">
-    <div >
-    <div  class=""style="background-color:white; margin-top:300px;">
-			<span class="name">{{ $project->company }}</span>
-	    <p class="card-text-title">{{$project->title}}</p>
-              <div class="on-off-div">
-	      	<div class="lang-block">
-		  <p class="card-text">HTML</p>
-	          @if($project->html == 1)
-                    <img src="img/green-{!! 'on' !!}.png">
-                    <img src="img/red-{!! 'off' !!}.png">
-                  @else
-                    <img src="img/green-{!! 'off' !!}.png">
-                    <img src="img/red-{!! 'on' !!}.png">
-                  @endif
-		</div>
-		<div class="lang-block">
-	      	  <p class="card-text">CSS</p>
-	          @if($project->css == 1)
-                    <img src="img/green-{!! 'on' !!}.png">
-                    <img src="img/red-{!! 'off' !!}.png">
-                  @else
-                    <img src="img/green-{!! 'off' !!}.png">
-                    <img src="img/red-{!! 'on' !!}.png">
-                  @endif
-		</div>
-		<div class="lang-block">
-	      	  <p class="card-text">Javascript</p>
-	          @if($project->javasrcipt == 1)
-                    <img src="img/green-{!! 'on' !!}.png">
-                    <img src="img/red-{!! 'off' !!}.png">
-                  @else
-                    <img src="img/green-{!! 'off' !!}.png">
-                    <img src="img/red-{!! 'on' !!}.png">
-                  @endif
-		</div>
-		<div class="lang-block">
-	      	  <p class="card-text">PHP</p>
-	          @if($project->php == 1)
-                    <img src="img/green-{!! 'on' !!}.png">
-                    <img src="img/red-{!! 'off' !!}.png">
-                  @else
-                    <img src="img/green-{!! 'off' !!}.png">
-                    <img src="img/red-{!! 'on' !!}.png">
-                 @endif
-		</div>
-              </div>
-              <p class="card-date">@php echo date('F, Y ',strtotime($project->year)); @endphp</p><br>
-	      <p class="desc-text">{{ $project->descr}}</p>
-	            </div>
-            </div>
-					</div>
-            </div>
+	<img src='{{ $project->img }}' class="" style="width:100%; border:1px solid black; border-radius:10px;">
+</div>
+<div >
+<div  class="">
+<p class="card-date">{{$project->year }}</p><br>
+<p class="desc-text">{{ $project->desc}}</p>
+</div>
+</div>
+</div>
+@if($project->link)
 </a>
-            </div>
+@endif
