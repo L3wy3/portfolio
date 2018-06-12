658955c0362247b7626a2317d31054ffc3ebe450
lewy <lstowning@outlook.com>
2018-06-13 00:05:33 +0100

    dding variables o controllers

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -7,9 +7,12 @@ use App\Project;
 
 class WorkController extends Controller
 {
-  public function index(){
+  public function indexx(Request $request){
     $projects = Project::oldest()->get();
     $pagetitle = "work";
-    return view('work.index', compact('projects', 'pagetitle'));
+    $tag = $request->query('tag');
+    $agency = $request->query('agency');
+    $year = $request->query('year');
+    return view('work.index', compact('projects', 'year', 'tag', 'agency', 'pagetitle'));
   }
 }
