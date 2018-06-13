eebe8611163670e418ce2d7e0fbfe08363b15d33
lewy <lstowning@outlook.com>
2018-06-13 13:56:44 +0100

    quick fix

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,5 +1,5 @@
 <div class="blog-post">
-  <div style="background-image:url('{{ $post->image }}'); background-size:cover; padding: 350px 150px 0 0;">
+  <div style="background-image:url('{{ $post->img }}'); background-size:cover; padding: 350px 150px 0 0;">
     <div style="max-width:450px;" class="blog-container @if($i == 1)blog-container-even @else blog-container-odd @endif    ">
       <a href='/blog/{{ $post->id}}'> <h2 class="blog-post-title">{{ $post->title }}</h2></a>
       <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }} by Laurence</p>
